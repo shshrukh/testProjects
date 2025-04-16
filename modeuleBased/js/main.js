c8372751313ae28453dfc2dc6555845d58fbc6c6
shshrukh <shshrukh.arbaz@gmail.com>
2025-04-16 18:32:42 +0500

    changes

---
+++ modeuleBased/js/main.js
@@ -0,0 +1,10 @@
+import { renderNav } from "./nav.js";
+console.log(renderNav());
+let value = renderNav();
+
+
+const abc = document.querySelector('.container');
+// console.log('hello');
+abc.appendChild(value);
+
+
---
+++ modeuleBased/js/nav.js
@@ -0,0 +1,12 @@
+
+
+
+
+const renderNav = ()=>{
+    const createNav = document.createElement('nav');
+    createNav.innerHTML = '<a href="#">home</a><a href="#">about</a><a href="#">customer care</a><a href="#">contact</a><a href="#">about us</a>';
+    return createNav;
+};
+
+// export{renderNav}
+export {renderNav}
